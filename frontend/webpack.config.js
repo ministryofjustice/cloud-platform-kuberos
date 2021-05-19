ee44f7c927e6f7f8d9fb721cb83cc191e0ceb979
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-05-19 10:12:31 +0100

    fix run dev

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -35,9 +35,6 @@ module.exports = {
     ]
   },
   devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-  },
   plugins: [
     new webpack.ProvidePlugin({
       '$': 'jquery'
